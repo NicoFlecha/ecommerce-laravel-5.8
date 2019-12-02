14d6dcaf1794066b88b944449ec839ba0a9da8da
Nico Flecha <nicoflechacontacto@gmail.com>
2019-12-01 21:58:06 -0300

    validaciones para Productos

--- app/Http/Controllers/ProductoController.php
+++ app/Http/Controllers/ProductoController.php
@@ -28,6 +28,29 @@ class ProductoController extends Controller
     // Método guardar
     public function guardar(Request $form) // Toma lo que venga por Post de la ruta '/productos/agregar'
     {
+
+      // Validaciones
+      $this->validate($form,
+      [
+        'nombre' => 'required|unique:productos',
+        'categoria_id' => 'required',
+        'precio' => 'required|numeric|min:1',
+        'cantidad' => 'required|min:1|integer',
+        'descripcion' => 'required|min:2',
+      ],
+      [
+        'nombre.required' => 'El producto debe tener un nombre.',
+        'nombre.unique' => 'El producto ya existe.',
+        'categoria_id.required' => 'El producto debe tener una categoria.',
+        'precio.required' => 'El producto debe tener un precio.',
+        'precio.numeric' => 'El precio del producto debe ser un número.',
+        'precio.min' => 'El precio mínimo es de $ :min.',
+        'cantidad.required' => 'Debe indicar el stock disponible del producto.',
+        'cantidad.min' => 'El stock mínimo es de :min producto.',
+        'cantidad.numeric' => 'El stock debe ser un número entero.',
+        'descripcion.required' => 'El producto debe tener una descripcion.',
+        'descripcion.min' => 'La descripción debe tener más de :min caracteres.',
+      ]);
       // Asigna los valores a un objeto de clase Producto
       $producto = new Producto;
       $producto->nombre = $form['nombre'];
--- resources/views/productoAgregar.blade.php
+++ resources/views/productoAgregar.blade.php
@@ -13,6 +13,9 @@
     @csrf
     <label for="nombre">Nombre del Producto:</label><br>
     <input type="text" name="nombre" id="nombre" value=""><br>
+    @error ('nombre')
+      {{$message}}<br>
+    @enderror
     <label for="categoria">¿A qué categoría corresponde?:</label><br>
     <select class="" name="categoria_id" id="categoria">
       <option value="" selected>Elija una opción</option>
@@ -22,12 +25,24 @@
         </option>
       @endforeach
     </select><br>
+    @error ('categoria_id')
+      {{$message}}<br>
+    @enderror
     <label for="precio">Precio (en pesos):</label><br>
     <input type="text" name="precio"><br>
+    @error ('precio')
+      {{$message}}<br>
+    @enderror
     <label for="descripcion">Describí el Producto:</label><br>
     <textarea name="descripcion" rows="8" cols="80"></textarea><br>
+    @error ('descripcion')
+      {{$message}}<br>
+    @enderror
     <label for="cantidad">Stock del Producto:</label><br>
     <input type="number" name="cantidad" value="1"><br>
+    @error ('cantidad')
+      {{$message}}<br>
+    @enderror
     <input type="submit" value="Guardar">
   </form>
 @endsection
