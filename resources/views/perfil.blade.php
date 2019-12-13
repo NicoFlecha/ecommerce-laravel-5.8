6d59ac14959f6b5b0c6e720f417279b65bc6546a
Your Name <you@example.com>
2019-12-13 10:01:46 -0300

    porfin tenemos el perfil

--- resources/views/perfil.blade.php
+++ resources/views/perfil.blade.php
@@ -7,7 +7,27 @@
 @section('css')
   '/css/login.css'
 @endsection
-
+@php
+  $userName = Auth::user()->username;
+  $nombre = Auth::user()->name;
+  $apellido = Auth::user()->apellido;
+  $email = Auth::user()->email;
+@endphp
 @section('principal')
- hola
+<div class="datos">
+  @php
+    $avatar = 'default.png';
+    if (Auth::user()->avatar) {
+      $avatar = Auth::user()->avatar;
+    }
+  @endphp
+  <img src="/storage/{{$avatar}}">
+  <div class="">
+    <h1>{{$nombre}}-{{$apellido}}</h1>
+    <p>{{$email}}</p>
+  </div>
+</div>
+<div class="editar">
+  <a class="editar" href="/perfil/editar">Editar perfil</a>
+</div>
 @endsection
