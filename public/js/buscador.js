766beb35309fb0d83cfc2e6ab5ae72a9f9d8602f
NicoFlecha <nicoflechacontacto@gmail.com>
2019-12-14 16:29:20 -0300

    cambio de archivo el js de buscador

---
+++ public/js/buscador.js
@@ -0,0 +1,25 @@
+//buscador en tiempo real
+window.addEventListener('load', function(){
+    var buscador = document.querySelector('#texto')
+    buscador.addEventListener('keyup', function(){
+      if(this.value.length >= 3){
+      fetch(`/api/search/${this.value}`
+     )
+      .then(function(response) {
+      return response.json();
+      })
+      .then(function(data){
+        data.forEach(function (producto) {
+          document.getElementById("resultados").innerHTML = document.getElementById("resultados").innerHTML + producto.nombre
+          if(buscador.value.length > 3){
+            document.getElementById("resultados").innerHTML = ''
+            document.getElementById("resultados").innerHTML = document.getElementById("resultados").innerHTML + producto.nombre
+          }
+        })
+      })
+    }
+      else {
+        document.getElementById("resultados").innerHTML = ''
+      }
+    })
+})
