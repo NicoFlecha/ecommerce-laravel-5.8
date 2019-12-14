4ced4bde9bc975f3edb92057e2263ae6dbdcad09
NicoFlecha <nicoflechacontacto@gmail.com>
2019-12-14 16:50:25 -0300

    resultados se reinicia con cada key up

--- public/js/buscador.js
+++ public/js/buscador.js
@@ -1,6 +1,7 @@
 //buscador en tiempo real
 window.addEventListener('load', function(){
-    var buscador = document.querySelector('#texto')
+    var buscador = document.querySelector('#texto');
+    var resultados = document.getElementById("resultados");
     buscador.addEventListener('keyup', function(){
       if(this.value.length >= 3){
       fetch(`/api/search/${this.value}`
@@ -9,12 +10,13 @@ window.addEventListener('load', function(){
       return response.json();
       })
       .then(function(data){
+        resultados.innerHTML = '';
         data.forEach(function (producto) {
-          document.getElementById("resultados").innerHTML = document.getElementById("resultados").innerHTML + producto.nombre
-          if(buscador.value.length > 3){
-            document.getElementById("resultados").innerHTML = ''
-            document.getElementById("resultados").innerHTML = document.getElementById("resultados").innerHTML + producto.nombre
-          }
+          resultados.innerHTML = resultados.innerHTML + producto.nombre
+          // if(buscador.value.length >= 3){
+          //   document.getElementById("resultados").innerHTML = ''
+          //   document.getElementById("resultados").innerHTML = document.getElementById("resultados").innerHTML + producto.nombre
+          // }
         })
       })
     }
